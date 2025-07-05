a39f487d955e5e1887427a91b6535d1afbadb0c9
Colin Thomas <colin@avaamo.com>
2025-07-05 19:11:57 +0200

    Adding install scripts to setup.sh

---
+++ setup.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+# Function to check if Homebrew is installed
+is_homebrew_installed() {
+  command -v brew >/dev/null 2>&1
+}
+
+# Homebrew
+if is_homebrew_installed; then
+	echo "Homebrew is already installed. Skipping installation."
+else
+	echo "Homebrew not found. Installing Homebrew..."
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	echo >> $HOME/.zprofile
+	echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+	eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+if ! brew list --formula | grep -q "^wget$"; then
+	brew install wget
+fi
+
+# Fonts
+echo 'Checking fonts'
+if ! brew list --formula | grep -q "^font-ubuntu-sans-nerd-font$"; then
+	brew install font-ubuntu-sans-nerd-font
+fi
+if ! brew list --formula | grep -q "^font-sauce-code-pro-nerd-font$"; then
+	brew install font-sauce-code-pro-nerd-font
+fi
+if ! brew list --formula | grep -q "^font-jetbrains-mono-nerd-font$"; then
+	brew install font-jetbrains-mono-nerd-font
+fi
+
+# bat
+if ! brew list --formula | grep -q "^bat$"; then
+	# Install bat
+	echo 'Installing bat'
+	brew install bat
+	# Setup bat
+	echo 'export BAT_CONFIG_PATH="$HOME/.config/bat/bat.conf"' >> $HOME/.zshenv
+	source $HOME/.zshenv
+	mkdir -p "$(bat --config-dir)/themes"
+	wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Mocha.tmTheme
+	bat cache --build
+	echo 'export BAT_THEME="Catppuccin Mocha"' >> $HOME/.zshenv
+	source $HOME/.zshenv
+fi
+
+# fzf
+if ! brew list --formula | grep -q "^fzf$"; then
+	brew install fzf
+fi
+
+# OMP
+if ! brew list --formula | grep -q "^oh-my-posh$"; then
+	brew install jandedobbeleer/oh-my-posh/oh-my-posh
+fi
+
+# Stow
+if ! brew list --formula | grep -q "^stow$"; then
+	brew install stow
+fi
+stow .
