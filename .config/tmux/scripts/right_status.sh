0a50e206bce4ea2d8c800da9cba85d07c71ece38
Colin Thomas <colin@avaamo.com>
2025-07-25 11:25:53 +0200

    Tmux right status now has a conditional check for rpi specific items

--- .config/tmux/scripts/right_status.sh
+++ .config/tmux/scripts/right_status.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 host=$(hostname)
-temp_raw=$(cat /sys/class/thermal/thermal_zone0/temp)
-temp_c=$(awk "BEGIN { printf \"%.1f\", $temp_raw / 1000 }")
+if [[ "$(cat /proc/device-tree/model 2>/dev/null)" == *"Raspberry Pi"* ]]; then
+  temp_raw=$(cat /sys/class/thermal/thermal_zone0/temp)
+  temp_c=$(awk "BEGIN { printf \"%.1f\", $temp_raw / 1000 }")
 
-if (( $(echo "$temp_c < 60" | bc -l) )); then
-  color="green"
-elif (( $(echo "$temp_c < 65" | bc -l) )); then
-  color="yellow"
+  if (($(echo "$temp_c < 60" | bc -l))); then
+    color="green"
+  elif (($(echo "$temp_c < 65" | bc -l))); then
+    color="yellow"
+  else
+    color="red"
+  fi
+  echo "#[fg=cyan]$host | #[fg=$color]$temp_c°C"
 else
-  color="red"
+  echo "#[fg=cyan]$host"
 fi
-
-echo "#[fg=cyan]$host | #[fg=$color]$temp_c°C"
-
