bedc2aa3d68bf88444c9e1576ce9d1b9980136d7
Colin Thomas <colin@avaamo.com>
2025-08-05 19:41:14 +0200

    Adding gitsigns to nvim

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -2,10 +2,32 @@ return {
     "lewis6991/gitsigns.nvim",
     config = function()
         require("gitsigns").setup({
-            signcolumn = true,
-            word_diff = true
+            signs = {
+        add          = { text = '┃' },
+        change       = { text = '┃' },
+        delete       = { text = '_' },
+        topdelete    = { text = '‾' },
+        changedelete = { text = '~' },
+        untracked    = { text = '┆' },
+      },
+      signs_staged = {
+        add          = { text = '┃' },
+        change       = { text = '┃' },
+        delete       = { text = '_' },
+        topdelete    = { text = '‾' },
+        changedelete = { text = '~' },
+        untracked    = { text = '┆' },
+      },
+      signs_staged_enable = true
         })
+
+      -- Customize highlight colors
+      local flavor = require("catppuccin").flavour or "mocha"
+      local cp = require("catppuccin.palettes").get_palette(flavor)
+
+      vim.api.nvim_set_hl(0, "GitSignsAdd",    { fg = cp.green })
+      vim.api.nvim_set_hl(0, "GitSignsChange", { fg = cp.yellow })
+      vim.api.nvim_set_hl(0, "GitSignsDelete", { fg = cp.red })
     end,
     event = "VeryLazy"
 }
-
