388afd258ab5c64d120c3284dffbcd947c183a6c
Colin Thomas <colin@avaamo.com>
2025-08-06 11:19:24 +0200

    Neovim options and autocmd for removing highlight on esc

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -2,6 +2,9 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- Nerd fonts
+vim.g.have_nerd_font = true
+
 -- Make line numbers default
 vim.o.number = true
 -- relative
@@ -19,5 +22,28 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
+-- Highlight the light the cursor is on
+vim.opt.cursorline = true
 
 vim.opt.termguicolors = true
+
+-- Don't show mode
+vim.opt.showmode = false
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.opt.confirm = true
+
+-- Search options
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Show a pane at the bottom for previewing search and replace operations
+vim.opt.inccommand = 'split'
+
+-- default border for floating windowd
+vim.opt.winborder = 'rounded'
