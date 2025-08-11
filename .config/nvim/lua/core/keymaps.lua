311686d38082c1844b91116227d3bacb6b760e02
Colin Thomas <colin@avaamo.com>
2025-08-11 11:54:09 +0200

    Neovim, adding keymap for yanking the entire buffer

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -1,3 +1,6 @@
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Keymap for yanking the complete buffer
+vim.keymap.set('n', 'aa', '<cmd>:%y<CR>', { desc = 'Yank full buffer' })
