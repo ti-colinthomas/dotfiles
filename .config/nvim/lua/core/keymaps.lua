9b93fc0d05fa89426fb169fd57445c6b8352be09
Colin Thomas <colin@avaamo.com>
2025-08-11 17:38:54 +0200

    Adding keymap for indenting buffer

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -3,6 +3,9 @@
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Keymap for yanking, changing and deleting the complete buffer
-vim.keymap.set('n', 'ya', '<cmd>:%y<CR>', { desc = '[Y]ank [a]ll' })
-vim.keymap.set('n', 'ca', '<cmd>:%c<CR><Esc>i', { desc = '[C]hange [a]ll' })
-vim.keymap.set('n', 'da', '<cmd>:%d<CR>', { desc = '[D]elete [a]ll' })
+vim.keymap.set('n', 'ya', '<cmd>:%y<CR>', { desc = 'Yank all' })
+vim.keymap.set('n', 'ca', '<cmd>:%c<CR><Esc>i', { desc = 'Change all' })
+vim.keymap.set('n', 'da', '<cmd>:%d<CR>', { desc = 'Delete all' })
+
+-- Indent the complete buffer
+vim.keymap.set('n', '==', '<cmd>:normal ggVG=<CR>', { desc = 'Indent buffer' })
