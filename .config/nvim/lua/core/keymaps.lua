cf5ee5dfe32630ca7732940e9ef3ab67d45f8bbd
Colin Thomas <colin@avaamo.com>
2025-08-11 12:17:59 +0200

    Neovim, changing the keymap to yank full buffer to yawq

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -3,4 +3,4 @@
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Keymap for yanking the complete buffer
-vim.keymap.set('n', 'aa', '<cmd>:%y<CR>', { desc = 'Yank full buffer' })
+vim.keymap.set('n', 'ya', '<cmd>:%y<CR>', { desc = 'Yank full buffer' })
