c18ca641d4007a7f00df6c1443aa35a465b4870b
Colin Thomas <colin@avaamo.com>
2025-08-11 17:08:32 +0200

    Fixing indent issues

--- multipass.sh
+++ multipass.sh
@@ -8,40 +8,40 @@ CPU_COUNT="2"
 
 # Separate arrays for host and guest paths
 HOST_PATHS=(
-    "/Users/colin/code"
-    "/Users/colin/dotfiles"
+  "/Users/colin/code"
+  "/Users/colin/dotfiles"
 )
 GUEST_PATHS=(
-    "/home/ubuntu/code"
-    "/home/ubuntu/dotfiles"
+  "/home/ubuntu/code"
+  "/home/ubuntu/dotfiles"
 )
 # --- End configuration --
 # --- For a basic setup, you don't need to change anything beyond this ---
 
 # List of additional APT packages to install
 APT_PACKAGES=(
-    "unzip"
-    "git"
-    "stow"
-    "bat"
-    "git-delta"
-    "fd-find"
-    "ripgrep"
-    "eza"
-    "jq"
-    "tmux"
-    "xsel"
-    "build-essential"
-    "cmake"
+  "unzip"
+  "git"
+  "stow"
+  "bat"
+  "git-delta"
+  "fd-find"
+  "ripgrep"
+  "eza"
+  "jq"
+  "tmux"
+  "xsel"
+  "build-essential"
+  "cmake"
 )
 
 check_error() {
-    if [ $? -ne 0 ]; then
-        echo -e "\033[0;31mERROR: $1\033[0m" >&2
-        multipass delete "$INSTANCE_NAME" &>/dev/null
-        multipass purge
-        exit 1
-    fi
+  if [ $? -ne 0 ]; then
+    echo -e "\033[0;31mERROR: $1\033[0m" >&2
+    multipass delete "$INSTANCE_NAME" &>/dev/null
+    multipass purge
+    exit 1
+  fi
 }
 
 # Define colors
@@ -52,47 +52,47 @@ NC='\033[0m'        # No Color
 
 # Logging function
 log() {
-    local loglevel="$1"
-    shift
-    local message="$@"
+  local loglevel="$1"
+  shift
+  local message="$@"
 
-    case "$loglevel" in
-        info)
-            echo -e "${GREEN}[INFO] ${message}${NC}"
-            ;;
-	debug)
-            echo -e "${YELLOW}[DEBUG] ${message}${NC}"
-            ;;
-        error)
-            echo -e "${RED}[ERROR] ${message}${NC}"
-            ;;
-        *)
-            echo "[UNKNOWN] $message"
-            ;;
-    esac
+  case "$loglevel" in
+    info)
+      echo -e "${GREEN}[INFO] ${message}${NC}"
+      ;;
+    debug)
+      echo -e "${YELLOW}[DEBUG] ${message}${NC}"
+      ;;
+    error)
+      echo -e "${RED}[ERROR] ${message}${NC}"
+      ;;
+    *)
+      echo "[UNKNOWN] $message"
+      ;;
+  esac
 }
 
 # Check if Instance is ready
 is_vm_ready() {
-    multipass info $INSTANCE_NAME | grep --silent --regexp "State:\s*Running"
+  multipass info $INSTANCE_NAME | grep --silent --regexp "State:\s*Running"
 }
 
 
 log info "Checking for existing Multipass instance: $INSTANCE_NAME "
 if multipass info "$INSTANCE_NAME" &>/dev/null; then
-    log info "Instance '$INSTANCE_NAME' already exists. Deleting it..."
-    multipass delete "$INSTANCE_NAME"
-    multipass purge
-    log info "Instance '$INSTANCE_NAME' deleted."
+  log info "Instance '$INSTANCE_NAME' already exists. Deleting it..."
+  multipass delete "$INSTANCE_NAME"
+  multipass purge
+  log info "Instance '$INSTANCE_NAME' deleted."
 fi
 
 # Ensure all host share paths exist
 for path in "${HOST_PATHS[@]}"; do
-    if [ ! -d "$path" ]; then
-        log info "Creating host share directory: $path"
-        mkdir -p "$path"
-        check_error "Failed to create host share directory '$path'."
-    fi
+  if [ ! -d "$path" ]; then
+    log info "Creating host share directory: $path"
+    mkdir -p "$path"
+    check_error "Failed to create host share directory '$path'."
+  fi
 done
 
 # Launch VM
@@ -106,13 +106,13 @@ multipass umount "$INSTANCE_NAME"
 # Mount folders
 log info "Mounting custom folders "
 for i in "${!HOST_PATHS[@]}"; do
-    host_path="${HOST_PATHS[$i]}"
-    guest_path="${GUEST_PATHS[$i]}"
-    echo "  Mounting $host_path to $guest_path"
-    multipass exec "$INSTANCE_NAME" -- sudo mkdir -p "$guest_path"
-    check_error "Failed to create guest directory '$guest_path'"
-    multipass mount "$host_path" "$INSTANCE_NAME":"$guest_path"
-    check_error "Failed to mount '$host_path' to '$guest_path'"
+  host_path="${HOST_PATHS[$i]}"
+  guest_path="${GUEST_PATHS[$i]}"
+  echo "  Mounting $host_path to $guest_path"
+  multipass exec "$INSTANCE_NAME" -- sudo mkdir -p "$guest_path"
+  check_error "Failed to create guest directory '$guest_path'"
+  multipass mount "$host_path" "$INSTANCE_NAME":"$guest_path"
+  check_error "Failed to mount '$host_path' to '$guest_path'"
 done
 
 log info "Updating and Upgrading packages "
@@ -126,68 +126,68 @@ multipass exec "$INSTANCE_NAME" -- sudo apt install -y zsh zsh-doc zsh-common
 check_error "Failed to install Zsh."
 
 multipass exec "$INSTANCE_NAME" -- bash -c '
-    CURRENT_SHELL=$(getent passwd ubuntu | cut -d: -f7)
-    if [ "$CURRENT_SHELL" != "$(which zsh)" ]; then
-        sudo chsh -s "$(which zsh)" ubuntu
-        echo "Zsh set as default shell."
-    else
-        echo "Zsh is already the default shell."
-    fi
+CURRENT_SHELL=$(getent passwd ubuntu | cut -d: -f7)
+if [ "$CURRENT_SHELL" != "$(which zsh)" ]; then
+  sudo chsh -s "$(which zsh)" ubuntu
+  echo "Zsh set as default shell."
+else
+  echo "Zsh is already the default shell."
+fi
 '
 check_error "Failed to set Zsh as default shell."
 
 log info "Adding repository for eza to API Sources"
 multipass exec "$INSTANCE_NAME" -- bash -c '
-    sudo mkdir -p /etc/apt/keyrings
-    wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
-    echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
-    sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
-    sudo apt update
+sudo mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+sudo apt update
 '
 check_error "Failed to add repository for eza"
 
 if [ ${#APT_PACKAGES[@]} -gt 0 ]; then
-    log info "Installing additional APT packages "
-    for pkg in "${APT_PACKAGES[@]}"; do
-        log info "  Installing $pkg..."
-        multipass exec "$INSTANCE_NAME" -- sudo apt install -y "$pkg"
-        check_error "Failed to install package '$pkg'."
-    done
+  log info "Installing additional APT packages "
+  for pkg in "${APT_PACKAGES[@]}"; do
+    log info "  Installing $pkg..."
+    multipass exec "$INSTANCE_NAME" -- sudo apt install -y "$pkg"
+    check_error "Failed to install package '$pkg'."
+  done
 else
-    log info "No additional APT packages specified."
+  log info "No additional APT packages specified."
 fi
 
 log info "Installing fzf "
 multipass exec "$INSTANCE_NAME" -- bash -c '
-    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-    ~/.fzf/install --all --no-bash 
-    rm ~/.zshrc
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --all --no-bash 
+rm ~/.zshrc
 '
 check_error "Failed to install fzf"
 
 log info "Installing Zoxide "
 multipass exec "$INSTANCE_NAME" -- bash -c '
-    curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
 '
 check_error "Failed to install zoxide"
 
 log info "Installing oh-my-posh "
 multipass exec "$INSTANCE_NAME" -- bash -c '
-    curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
+curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
 
 '
 check_error "Failed to install oh-my-posh"
 
 log info "Writing .zprofile "
 multipass exec "$INSTANCE_NAME" -- bash -c '
-    echo "# Custom .zprofile settings" >> ~/.zprofile
-    echo "export PATH=\"\$HOME/.local/bin:\$PATH\"" >> ~/.zprofile
-    echo "export EDITOR=\"vim\"" >> ~/.zprofile
-    echo "# Setup fzf" >> ~/.zprofile
-    echo "[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh" >> ~/.zprofile
-    echo "nvm use --lts" >> ~/.zprofile
-    echo "" >> ~/.zprofile
-    echo "echo \"Welcome to your Multipass Dev Environment!\"" >> ~/.zprofile
+echo "# Custom .zprofile settings" >> ~/.zprofile
+echo "export PATH=\"\$HOME/.local/bin:\$PATH\"" >> ~/.zprofile
+echo "export EDITOR=\"vim\"" >> ~/.zprofile
+echo "# Setup fzf" >> ~/.zprofile
+echo "[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh" >> ~/.zprofile
+echo "nvm use --lts" >> ~/.zprofile
+echo "" >> ~/.zprofile
+echo "echo \"Welcome to your Multipass Dev Environment!\"" >> ~/.zprofile
 '
 check_error "Failed to write .zprofile."
 
@@ -195,83 +195,83 @@ log info "Finalizing Setup "
 
 log debug "Stowing config files"
 multipass exec "$INSTANCE_NAME" -- bash -c '
-    echo "Stowing from : $(pwd)"
-    stow .
+echo "Stowing from : $(pwd)"
+stow .
 '
 
 log info "Applying catppuccin theme for bat"
 multipass exec "$INSTANCE_NAME" -- bash -c '
-    export BAT_CONFIG_PATH="$HOME/.config/bat/bat.conf"
-    mkdir -p "$(batcat --config-dir)/themes"
-    wget -P "$(batcat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Mocha.tmTheme
-    batcat cache --build
+export BAT_CONFIG_PATH="$HOME/.config/bat/bat.conf"
+mkdir -p "$(batcat --config-dir)/themes"
+wget -P "$(batcat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Mocha.tmTheme
+batcat cache --build
 '
 check_error "Failed to apply theme for bat"
 
 log info "Applying catppuccin theme for delta"
 multipass exec "$INSTANCE_NAME" -- bash -c '
-    mkdir -p "$HOME/.config/delta/themes"
-    wget -P "$HOME/.config/delta/themes" https://raw.githubusercontent.com/catppuccin/delta/refs/heads/main/catppuccin.gitconfig
+mkdir -p "$HOME/.config/delta/themes"
+wget -P "$HOME/.config/delta/themes" https://raw.githubusercontent.com/catppuccin/delta/refs/heads/main/catppuccin.gitconfig
 '
 check_error "Failed to apply theme for delta"
 
 log info "Applying catppuccin theme for eza"
 multipass exec "$INSTANCE_NAME" -- bash -c '
-    mkdir -p "$HOME/.config/eza"
-    wget -O "$HOME/.config/eza/theme.yml" https://raw.githubusercontent.com/eza-community/eza-themes/refs/heads/main/themes/catppuccin.yml
+mkdir -p "$HOME/.config/eza"
+wget -O "$HOME/.config/eza/theme.yml" https://raw.githubusercontent.com/eza-community/eza-themes/refs/heads/main/themes/catppuccin.yml
 
 '
 check_error "Failed to apply theme for eza"
 
 log info "Setting up the preview action for fzf"
 multipass exec "$INSTANCE_NAME" -- bash -c '
-    chmod +x ~/.config/fzf/preview.sh
+chmod +x ~/.config/fzf/preview.sh
 '
 check_error "Failed to setup the preview action for fzf"
 
 log info "Setting up tpm for tmux"
 multipass exec "$INSTANCE_NAME" -- bash -c '
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-    if [ -d "$HOME/.config/tmux/themes" ]; then
-	echo "Tmux theme already exists"
-    else
-	echo "Create themes directory for tmux"
-	mkdir -p "$HOME/.config/tmux/themes"
-	git clone -b v2.1.3 https://github.com/catppuccin/tmux.git $HOME/.config/tmux/themes
-    fi
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+if [ -d "$HOME/.config/tmux/themes" ]; then
+  echo "Tmux theme already exists"
+else
+  echo "Create themes directory for tmux"
+  mkdir -p "$HOME/.config/tmux/themes"
+  git clone -b v2.1.3 https://github.com/catppuccin/tmux.git $HOME/.config/tmux/themes
+fi
 '
 check_error "Failed to setup tpm"
 
 log info "Installing nvm"
 multipass exec "$INSTANCE_NAME" -- bash -c '
-    wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 '
 check_error "Failed to install nvm"
 
 log info "APT sources do not contain latest release of neovim. Script will now attempt to build neovim from source."
 multipass exec "$INSTANCE_NAME" -- bash -c '
-    git clone https://github.com/neovim/neovim $HOME/build/neovim
-    cd $HOME/build/neovim
-    git checkout stable
-    make CMAKE_BUILD_TYPE=RelWithDebInfo
-    cd build && cpack -G DEB && sudo dpkg -i nvim-linux-$(dpkg --print-architecture).deb
-    cd $HOME && rm -rf $HOME/build
+git clone https://github.com/neovim/neovim $HOME/build/neovim
+cd $HOME/build/neovim
+git checkout stable
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+cd build && cpack -G DEB && sudo dpkg -i nvim-linux-$(dpkg --print-architecture).deb
+cd $HOME && rm -rf $HOME/build
 '
 check_error "Failed to install neovim"
 
 log info "Restarting VM"
 multipass exec "$INSTANCE_NAME" -- bash -c '
-    sudo reboot
+sudo reboot
 '
 log info "Waiting for VM to be ready."
 while ! is_vm_ready; do
-    sleep 5
+  sleep 5
 done
 
 log info "Setup complete for $INSTANCE_NAME!"
 log info "You can connect using: multipass shell $INSTANCE_NAME"
 log info "Mounted directories:"
 for i in "${!HOST_PATHS[@]}"; do
-    echo "  ${HOST_PATHS[$i]} -> ${GUEST_PATHS[$i]}"
+  echo "  ${HOST_PATHS[$i]} -> ${GUEST_PATHS[$i]}"
 done
 
--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # Stow
 # Creates the directory structure
 if ! brew list --formula | grep -q "^stow$"; then
-	brew install stow
+  brew install stow
 fi
 stow .
 
@@ -23,127 +23,127 @@ is_homebrew_installed() {
 
 # Homebrew
 if is_homebrew_installed; then
-	echo "Homebrew is already installed. Skipping installation."
+  echo "Homebrew is already installed. Skipping installation."
 else
-	echo "Homebrew not found. Installing Homebrew..."
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-	echo >> $HOME/.zprofile
-	echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
-	eval "$(/opt/homebrew/bin/brew shellenv)"
+  echo "Homebrew not found. Installing Homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo >> $HOME/.zprofile
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 if ! brew list --formula | grep -q "^wget$"; then
-	brew install wget
+  brew install wget
 fi
 
 # Fonts
 echo 'Checking fonts'
 if ! brew list --cask | grep -q "^font-ubuntu-sans-nerd-font"; then
-	brew install font-ubuntu-sans-nerd-font
+  brew install font-ubuntu-sans-nerd-font
 fi
 if ! brew list --cask | grep -q "^font-sauce-code-pro-nerd-font"; then
-	brew install font-sauce-code-pro-nerd-font
+  brew install font-sauce-code-pro-nerd-font
 fi
 if ! brew list --cask | grep -q "^font-jetbrains-mono-nerd-font$"; then
-	brew install font-jetbrains-mono-nerd-font
+  brew install font-jetbrains-mono-nerd-font
 fi
 
 echo 'Installing and configuring tools'
 echo 'bat'
 # bat
 if ! brew list --formula | grep -q "^bat$"; then
-	# Install bat
-	echo 'Installing bat'
-	brew install bat
-	# Setup bat
-	mkdir -p "$(bat --config-dir)/themes"
-	wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Mocha.tmTheme
-	bat cache --build
+  # Install bat
+  echo 'Installing bat'
+  brew install bat
+  # Setup bat
+  mkdir -p "$(bat --config-dir)/themes"
+  wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Mocha.tmTheme
+  bat cache --build
 fi
 
 echo 'delta'
 # delta
 if ! brew list --formula | grep -q "^git-delta$"; then
-	brew install git-delta
-	mkdir -p "$CONFIG_DIR/delta/themes"
-	wget -P "$CONFIG_DIR/delta/themes" https://raw.githubusercontent.com/catppuccin/delta/refs/heads/main/catppuccin.gitconfig 
+  brew install git-delta
+  mkdir -p "$CONFIG_DIR/delta/themes"
+  wget -P "$CONFIG_DIR/delta/themes" https://raw.githubusercontent.com/catppuccin/delta/refs/heads/main/catppuccin.gitconfig 
 fi
 
 echo 'fzf'
 # fzf
 if ! brew list --formula | grep -q "^fzf$"; then
-	brew install fzf
-	chmod +x ~/.config/fzf/preview.sh
+  brew install fzf
+  chmod +x ~/.config/fzf/preview.sh
 fi
 
 echo 'fd'
 # fd
 if ! brew list --formula | grep -q "^fd$"; then
-	brew install fd
+  brew install fd
 fi
 
 echo 'oh-my-posh'
 # OMP
 if ! brew list --formula | grep -q "^oh-my-posh$"; then
-	brew install jandedobbeleer/oh-my-posh/oh-my-posh
+  brew install jandedobbeleer/oh-my-posh/oh-my-posh
 fi
 
 echo 'eza'
 # Eza
 if ! brew list --formula | grep -q "^eza$"; then
-	brew install eza
-	# Configure eza
-	mkdir -p "$CONFIG_DIR/eza"
-	wget -O "$CONFIG_DIR/eza/theme.yml" https://raw.githubusercontent.com/eza-community/eza-themes/refs/heads/main/themes/catppuccin.yml
+  brew install eza
+  # Configure eza
+  mkdir -p "$CONFIG_DIR/eza"
+  wget -O "$CONFIG_DIR/eza/theme.yml" https://raw.githubusercontent.com/eza-community/eza-themes/refs/heads/main/themes/catppuccin.yml
 fi
 
 echo 'jq'
 # jq
 if ! brew list --formula | grep -q "^jq$"; then
-	brew install jq
+  brew install jq
 fi
 
 echo 'zoxide'
 # Zoxide
 if ! brew list --formula | grep -q "^zoxide$"; then
-	brew install zoxide
+  brew install zoxide
 fi
 
 echo 'nvm'
 # Nvm
 if ! brew list --formula | grep -q "^nvm$"; then
-	brew install nvm
+  brew install nvm
 fi
 
 echo 'Aerospace'
 # Aerospace
 if ! brew list --cask | grep -q "^aerospace$"; then
-	brew install --cask nikitabobko/tap/aerospace
-	
-	# Disabling this for now
-	# Mutliple monitors cause issues that I just don't want to deal with right now
-	#
-	# chmod +x ./config/aerospace/scripts/disable_displays_have_seperate_spaces.sh
-	# /bin/bash -c ./config/aerospace/scripts/disable_displays_have_seperate_spaces.sh
+  brew install --cask nikitabobko/tap/aerospace
+
+  # Disabling this for now
+  # Mutliple monitors cause issues that I just don't want to deal with right now
+  #
+  # chmod +x ./config/aerospace/scripts/disable_displays_have_seperate_spaces.sh
+  # /bin/bash -c ./config/aerospace/scripts/disable_displays_have_seperate_spaces.sh
 fi
 
 echo 'Ripgrep'
 # Ripgrep
 if ! brew list --formula | grep -q "^ripgrep$"; then
-	brew install ripgrep
+  brew install ripgrep
 fi
 
 echo 'Tmux'
 # Tmux
 if ! brew list --formula | grep -q "^tmux$"; then
-	brew install tmux
-	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-	mkdir "$CONFIG_DIR/tmux/themes"
-	git clone -b v2.1.3 https://github.com/catppuccin/tmux.git $CONFIG_DIR/tmux/themes
+  brew install tmux
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  mkdir "$CONFIG_DIR/tmux/themes"
+  git clone -b v2.1.3 https://github.com/catppuccin/tmux.git $CONFIG_DIR/tmux/themes
 fi
 
 echo 'neovim'
 # Neovim
 if ! brew list --formula | grep -q "^neovim$"; then
-	brew install neovim
+  brew install neovim
 fi
