74d152652e7166151b0bca0a2bf9e772d1877c71
Colin Thomas <colin@avaamo.com>
2025-08-12 15:14:13 +0200

    Neovim, changing keymap for yanking, changing, deleting contents of buffer

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -3,9 +3,9 @@
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Keymap for yanking, changing and deleting the complete buffer
-vim.keymap.set('n', 'ya', '<cmd>:%y<CR>', { desc = 'Yank all' })
-vim.keymap.set('n', 'ca', '<cmd>:%c<CR><Esc>i', { desc = 'Change all' })
-vim.keymap.set('n', 'da', '<cmd>:%d<CR>', { desc = 'Delete all' })
+vim.keymap.set('n', 'yaa', '<cmd>:%y<CR>', { desc = 'Yank all' })
+vim.keymap.set('n', 'caa', '<cmd>:%c<CR><Esc>i', { desc = 'Change all' })
+vim.keymap.set('n', 'daa', '<cmd>:%d<CR>', { desc = 'Delete all' })
 
 -- Indent the complete buffer
 vim.keymap.set('n', '==', 'mzgg=G`z', { desc = 'Indent buffer' })
