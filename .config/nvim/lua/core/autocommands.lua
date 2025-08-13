a31faa3dc35c2b591060977f8ecf77fe69216d32
Colin Thomas <colin@avaamo.com>
2025-08-13 22:10:51 +0200

    Neovim, added an autocommand to dynamically rename tmux based on filename in neovim

--- .config/nvim/lua/core/autocommands.lua
+++ .config/nvim/lua/core/autocommands.lua
@@ -1,3 +1,8 @@
+-- Function to check if neovim is running in tmux
+local function is_running_in_tmux()
+  return os.getenv("TMUX") ~= nil
+end
+
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.hl.on_yank()`
@@ -11,20 +16,37 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 -- Enable the sign column when in a git repository
 vim.api.nvim_create_autocmd("BufEnter", {
-    callback = function()
-        local git_dir = vim.fn.finddir(".git", ".;")
-        if git_dir ~= "" then
-            vim.opt.signcolumn = "yes"
-        else
-            vim.opt.signcolumn = "auto"
-        end
-    end,
+  callback = function()
+    local git_dir = vim.fn.finddir(".git", ".;")
+    if git_dir ~= "" then
+      vim.opt.signcolumn = "yes"
+    else
+      vim.opt.signcolumn = "auto"
+    end
+  end,
+  desc = "Enable the sign column when in a git repository"
 })
+
+-- Rename the tmux window when a file is opened in neovim
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function()
+    -- Rename the tmux window based on file opened in neovim
+    if is_running_in_tmux() then
+      local filename = vim.fn.expand('%:t')  -- Get the current file name
+      if filename ~= '' then
+        os.execute("tmux rename-window " .. filename)
+      end
+    end
+  end,
+  desc = "Change tmux window name based on the opened file"
+})
+
+
 
 -- -- Callback on exiting vim
 -- vim.api.nvim_create_autocmd("VimLeave", {
---     callback = function()
---         -- Reset cursor to blinking line (adjust for your terminal)
---         vim.fn.system("printf '\\e[5 q'")
---     end,
+--   callback = function()
+--     -- Reset cursor to blinking line (adjust for your terminal)
+--     vim.fn.system("printf '\\e[5 q'")
+--   end,
 -- })
