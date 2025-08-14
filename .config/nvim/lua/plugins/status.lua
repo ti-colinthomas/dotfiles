323ff9ce0c04966514e0047acab5ec385839fc30
Colin Thomas <colin@avaamo.com>
2025-08-14 17:50:48 +0200

    Tmux and Neovim, status line integration

--- .config/nvim/lua/plugins/status.lua
+++ .config/nvim/lua/plugins/status.lua
@@ -10,13 +10,14 @@ return {
     lualine.setup({
       options = {
         theme = "catppuccin",
+        section_separators = { left = "", right = "" },
+        component_separators = { left = "", right = "" }
       },
       sections = {
         lualine_a = {"mode"},
         lualine_b = {"branch", "diff", "diagnostics"},
         lualine_c = {},
         lualine_x = {"filetype"},
-        -- lualine_x = {"encoding", "fileformat", "filetype"},
         lualine_y = {},
         lualine_z = {"location"}
       },
