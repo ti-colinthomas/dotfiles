608e9b39b5fb69ee50a0b8dc98bd25516c6f0296
Colin Thomas <colin@avaamo.com>
2025-08-15 20:09:24 +0200

    Neovim, lualine is back and configured

---
+++ .config/nvim/lua/plugins/status.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function ()
+      -- import tmux_utils
+      local tmux_utils = require("custom.tmux_utils")
+
+      -- Set the b section for lualine
+      local section_b = {}
+      if tmux_utils.is_tmux_active() then
+        section_b = {tmux_utils.tmux_window_name, "branch", "diff", "diagnostics"}
+      else
+        section_b = {"branch", "diff", "diagnostics"}
+      end
+
+      local lualine = require("lualine")
+      lualine.setup({
+        options = {
+          theme = "auto",
+          section_separators = { left = "", right = "" },
+          component_separators = { left = "", right = "" }
+        },
+        sections = {
+          lualine_a = {"mode"},
+          lualine_b = section_b,
+          lualine_c = {},
+          lualine_x = {"filetype"},
+          lualine_y = {},
+          lualine_z = {"location"}
+        }
+      })
+    end
+  }
+}
